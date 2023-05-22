bf9230a91ae55dd6615da13798ae4fe78f53b35e
Atlegang Sethono <atlegangsethono@gmail.com>
2023-05-22 13:12:28 +0200

    Add functions to process API JSON data and extract required app data

--- src/api.js
+++ src/api.js
@@ -1,6 +1,30 @@
+// Create function that processes JSON data and returns the object for the required data
+function selectedWeatherData(weatherData) {
+    const {
+        location: { name: locationName, country },
+        current: {
+            temp_c: temperatureCelsius,
+            condition: { text: conditionText },
+            humidity,
+            wind_kph: windSpeedKph,
+            wind_dir: windDirection,
+        },
+    } = weatherData;
+
+  return {
+    location: { name: locationName, country },
+    current: {
+      temperature: { celsius: temperatureCelsius },
+      condition: conditionText,
+      humidity,
+      wind: { speed_kph: windSpeedKph, direction: windDirection },
+    },
+  };
+}
+
+
 // Create function that hits API server
 // Function takes location(city) and returns weather data for said city
-
 export async function fetchWeatherData(city) {
     try {
         const apiKey = 'f7d08dafee3f42bd92c101550231105';
@@ -10,12 +34,13 @@ export async function fetchWeatherData(city) {
         const response = await fetch(apiURL, { mode: 'cors' });
         const data = await response.json();
 
+        const selectedData = selectedWeatherData(data);
         // Log json in console
-        console.log('Weather data:', data);
+        console.log('Selected weather data:', selectedData);
 
         // Return data
         return data;
-    }   catch(error) {
+    }   catch (error) {
         // Handle any errors that occur during the search   
         console.error('Error fetching weather data', error);
     }   
